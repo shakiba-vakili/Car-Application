2cb394981b8555f71f78044a9570f563da3f3d5f
Shakiba Sadat Vakili <122327036+shakiba-vakili@users.noreply.github.com>
2025-08-08 15:43:02 +0330

    Add comment explaining MouseEventHandler typing in CustomButton

---
+++ types/index.ts
@@ -0,0 +1,27 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  // it could be optional
+  containerStyles?: string;
+  handleClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+/**
+ * 🔹 What is MouseEventHandler<HTMLButtonElement>?
+ *
+ * It means: this is a function that handles mouse events (like clicks)
+ * on a <button> element in React with TypeScript.
+ *
+ * Example:
+ * const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+ *   console.log("Button clicked!", e.currentTarget);
+ * };
+ *
+ * It's better than just using () => void because:
+ * - You get autocomplete for the event object
+ * - TypeScript will catch mistakes if the function is used on the wrong element
+ *
+ * If you want this function to be optional:
+ * handleClick?: MouseEventHandler<HTMLButtonElement>
+ */
